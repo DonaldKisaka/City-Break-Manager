15a2dd6dcc2b7ae19fed35b45725a742f2a54738
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-26 19:43:33 +0300

    Repository

--- city/src/main/java/com/example/citybreak/city/repository/CityRepository.java
+++ city/src/main/java/com/example/citybreak/city/repository/CityRepository.java
@@ -1,9 +1,19 @@
 package com.example.citybreak.city.repository;
 
 import com.example.citybreak.city.model.City;
+import com.example.citybreak.city.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CityRepository extends JpaRepository<City, Long> {
 
     boolean existsByName(String name);
+
+    List<City> findByUserId(Long userId);
+    boolean existsByNameAndUserId(String name, Long userId);
+    Optional<City> findByIdAndUserId(Long id, Long userId);
+
+    Long user(User user);
 }
--- city/src/main/java/com/example/citybreak/city/repository/TripRepository.java
+++ city/src/main/java/com/example/citybreak/city/repository/TripRepository.java
@@ -4,8 +4,13 @@ import com.example.citybreak.city.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
     List<Trip> findByCityId(Long cityId);
+
+    List<Trip> findByUserId(Long userId);
+    List<Trip> findByCityIdAndUserId(Long cityId, Long userId);
+    Optional<Trip> findByIdAndUserId(Long id, Long userId);
 
 }
